1a0232d51db0c3e7edbbbbb9f607453b8495f504
DAN <58280710+daniel081009@users.noreply.github.com>
2022-12-30 09:31:14 +0900

    super OK

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,31 +1,32 @@
-const staticAssets = ["/", "./js/main.js"];
-self.addEventListener("install", async (event) => {
-  const cache = await caches.open("static-cache");
-  cache.addAll(staticAssets);
+const sCacheName = "practice-pwa";
+const aFilesToCache = ["./", "./index.html", "./css/main.css", "./js/main.js"];
+
+self.addEventListener("install", (pEvent) => {
+  console.log("서비스워커 설치");
+  pEvent.waitUntil(
+    caches.open(sCacheName).then((pCache) => {
+      return pCache.addAll(aFilesToCache);
+    })
+  );
+});
+// 고유번호 할당받은 서비스 워커 동작 시작
+self.addEventListener("activate", (pEvent) => {
+  console.log("서비스워커 동작!");
 });
 
-self.addEventListener("fetch", (event) => {
-  const req = event.request;
-  const url = new URL(req.url);
-
-  if (url.origin === location.url) {
-    event.respondWith(cacheFirst(req));
-  } else {
-    event.respondWith(newtorkFirst(req));
-  }
+//데이터 요청시 네트워크 또는 캐시에서 찾아 반환
+self.addEventListener("fetch", (pEvent) => {
+  pEvent.respondWith(
+    caches
+      .match(pEvent.request)
+      .then((response) => {
+        if (!response) {
+          console.log("네트워크에서 데이터 요청!", pEvent.request);
+          return fetch(pEvent.request);
+        }
+        console.log("캐시에서 데이터 요청!", pEvent.request);
+        return response;
+      })
+      .catch((err) => console.log(err))
+  );
 });
-async function cacheFirst(req) {
-  const cachedResponse = caches.match(req);
-  return cachedResponse || fetch(req);
-}
-async function newtorkFirst(req) {
-  const cache = await caches.open("dynamic-cache");
-
-  try {
-    const res = await fetch(req);
-    cache.put(req, res.clone());
-    return res;
-  } catch (error) {
-    return await cache.match(req);
-  }
-}
