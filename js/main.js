09bfff653a470d2d635bc85f3770f8851d563ddd
DAN <58280710+daniel081009@users.noreply.github.com>
2022-12-30 11:39:41 +0900

    asdf

--- js/main.js
+++ js/main.js
@@ -2,35 +2,62 @@ class UI {
   constructor(body) {
     this.body = body;
     this.timer = null;
-    this.Add = new Add();
+    this.Chose = new Add();
 
     window.onload = () => {
-      this.AddMainPage();
+      this.MainPage();
     };
   }
-  AddMainPage() {
+
+  MainPage() {
+    function AllChclear() {
+      document.body.querySelector("#plus").classList = ["item"];
+      document.body.querySelector("#sub").classList = ["item"];
+      document.body.querySelector("#mult").classList = ["item"];
+      document.body.querySelector("#division").classList = ["item"];
+    }
     this.body.innerHTML = `
-      <h1>ADD Game</h1>
-      <div id="start">Start</div>
+    <h1>Math Game</h1>
+    <div class="grid">
+      <div class="item ch" id="plus">+</div>
+      <div class="item" id="sub">-</div>
+      <div class="item" id="mult">✕</div>
+      <div class="item" id="division">÷</div>
+    </div>
+    <div id="start">Start</div>
     `;
-    document.body.querySelector("#start").onclick = () => {
-      this.Add.score = 0;
-      this.AddGamePage();
-      this.timer = setTimeout(() => {
-        this.AddEndPage();
-      }, 1000 * 60);
+    document.body.querySelector("#plus").onclick = () => {
+      this.Chose = new Add();
+      AllChclear();
+      document.body.querySelector("#plus").classList.add("ch");
     };
+    document.body.querySelector("#sub").onclick = () => {
+      this.Chose = new subtraction();
+      AllChclear();
+      document.body.querySelector("#sub").classList.add("ch");
+    };
+    document.body.querySelector("#mult").onclick = () => {
+      this.Chose = new multiplication();
+      AllChclear();
+      document.body.querySelector("#mult").classList.add("ch");
+    };
+    document.body.querySelector("#division").onclick = () => {
+      this.Chose = new division();
+      AllChclear();
+      document.body.querySelector("#division").classList.add("ch");
+    };
+    this.startbutton();
   }
-  AddGamePage() {
+  GamePage() {
     this.body.innerHTML = `
     <div id= "plus">A + B</div>
     <input id="input" type="number" />
     `;
-    document.getElementById("plus").innerText = this.Add.Make();
+    document.getElementById("plus").innerText = this.Chose.Make();
     document.getElementById("input").onkeypress = (e) => {
       if (e.key === "Enter") {
-        if (this.Add.Check(document.getElementById("input").value)) {
-          document.getElementById("plus").innerText = this.Add.Make();
+        if (this.Chose.Check(document.getElementById("input").value)) {
+          document.getElementById("plus").innerText = this.Chose.Make();
           document.getElementById("plus").style.color = "";
         } else {
           document.getElementById("plus").style.color = "red";
@@ -39,21 +66,36 @@ class UI {
       }
     };
   }
-  AddEndPage() {
+  EndPage() {
     this.body.innerHTML = `
       <h1>ADD Game End</h1>
       <div class="end"></div>
       <div id="start">Start</div>
     `;
-    console.log(this.Add.score);
-    document.body.querySelector(".end").innerText = this.Add.score;
-    document.body.querySelector("#start").onclick = () => {
-      this.Add.score = 0;
-      this.AddGamePage();
-      this.timer = setTimeout(() => {
-        this.AddEndPage();
-      }, 1000 * 60);
+    console.log(this.Chose.score);
+    document.body.querySelector(".end").innerText = this.Chose.score;
+    this.startbutton();
+  }
+  startbutton() {
+    if (this.Chose == null) {
+      return;
+    }
+    document.onkeypress = (e) => {
+      console.log(e.key);
+      if (e.key === "spacebar") {
+        this.start();
+      }
     };
+    document.body.querySelector("#start").onclick = () => {
+      this.start();
+    };
+  }
+  start() {
+    this.Chose.score = 0;
+    this.GamePage();
+    this.timer = setTimeout(() => {
+      this.EndPage();
+    }, 1000 * 60);
   }
 }
 
@@ -82,4 +124,80 @@ class Add {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
+class subtraction {
+  constructor() {
+    this.level = 10;
+    this.score = 0;
+    this.Ok = 0;
+  }
+  Check(temp_Ok) {
+    if (this.Ok == temp_Ok) {
+      this.score++;
+      return true;
+    } else {
+      this.score--;
+      return false;
+    }
+  }
+  Make() {
+    let a = this.RandomInt(0, this.level);
+    let b = this.RandomInt(0, this.level);
+    this.Ok = a - b;
+    return a + " - " + b + " = ?";
+  }
+  RandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
+class multiplication {
+  constructor() {
+    this.level = 10;
+    this.score = 0;
+    this.Ok = 0;
+  }
+  Check(temp_Ok) {
+    if (this.Ok == temp_Ok) {
+      this.score++;
+      return true;
+    } else {
+      this.score--;
+      return false;
+    }
+  }
+  Make() {
+    let a = this.RandomInt(0, this.level);
+    let b = this.RandomInt(0, this.level);
+    this.Ok = a * b;
+    return a + " ⨯ " + b + " = ?";
+  }
+  RandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
+class division {
+  constructor() {
+    this.level = 10;
+    this.score = 0;
+    this.Ok = 0;
+  }
+  Check(temp_Ok) {
+    if (this.Ok == temp_Ok) {
+      this.score++;
+      return true;
+    } else {
+      this.score--;
+      return false;
+    }
+  }
+  Make() {
+    let a = this.RandomInt(0, this.level);
+    let b = this.RandomInt(0, this.level);
+    this.Ok = a / b;
+    return a + " / " + b + " = ?";
+  }
+  RandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
+
 const Ui = new UI(document.getElementById("main"));
