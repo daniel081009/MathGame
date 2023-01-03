fefb4a2f7081e77f5797696a88a2677ba6e9cfad
DAN <58280710+daniel081009@users.noreply.github.com>
2023-01-03 12:16:21 +0900

    PWA

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js");
-}
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js"
+);
+
+const {
+  cacheableResponse,
+  core,
+  expiration,
+  googleAnalytics,
+  routing,
+  strategies,
+} = workbox;
+const { CacheableResponsePlugin } = cacheableResponse;
+const { ExpirationPlugin } = expiration;
+const { registerRoute } = routing;
+const { CacheFirst, StaleWhileRevalidate } = strategies;
+
+core.clientsClaim();
+core.skipWaiting();
+
+// Cache JavaScript and CSS
+registerRoute(/\.(?:js|css)$/, new StaleWhileRevalidate());
+
+// Cache Images
+registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  new CacheFirst({
+    cacheName: "images",
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 10,
+        maxAgeSeconds: 60, // 7 Days
+      }),
+    ],
+  })
+);
