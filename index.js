1c2b5b5088cf3a5740d6457cab3e8f1552ddc4d0
DAN <58280710+daniel081009@users.noreply.github.com>
2023-01-03 12:02:46 +0900

    PWA update

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 if ("serviceWorker" in navigator) {
-  try {
-    navigator.serviceWorker.register("./serviceWorker.js");
-    console.log("Service Worker Registered");
-  } catch (error) {
-    console.log("Service Worker Registration Failed");
-  }
+  navigator.serviceWorker.register("/sw.js");
 }
