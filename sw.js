ff9225a0c121a2b9bd3c9b44f559173946204a0d
DAN <58280710+daniel081009@users.noreply.github.com>
2023-01-08 14:34:50 +0900

    update pwa

--- sw.js
+++ sw.js
@@ -3,34 +3,8 @@ importScripts(
 );
 
 workbox.routing.registerRoute(
-  /index\.html/,
+  new RegExp(".*"),
   workbox.strategies.networkFirst({
-    cacheName: "workbox:html",
-  })
-);
-
-workbox.routing.registerRoute(
-  new RegExp(".*.js"),
-  workbox.strategies.networkFirst({
-    cacheName: "workbox:js",
-  })
-);
-workbox.routing.registerRoute(
-  /.*\.css/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: "workbox:css",
-  })
-);
-
-workbox.routing.registerRoute(
-  /.*\.(?:png|jpg|jpeg|svg|gif)/,
-  workbox.strategies.cacheFirst({
-    cacheName: "workbox:image",
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 20,
-        maxAgeSeconds: 7 * 24 * 60 * 60,
-      }),
-    ],
+    cacheName: "all",
   })
 );
